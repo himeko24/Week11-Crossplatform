a114aa8a132fb19ed500404aab85e23f190fbbf9
himeko24 <stefanusandrian4@gmail.com>
2024-11-26 18:12:49 +0700

    added for w11

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const addData = async ()=>{
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            first: "Stefanus",
+            last: "Andrian",
+            born: "2003"
+        });
+        console.log("Document written with ID: ", docRef.id);
+    }catch(err){
+        console.error("Error occured ", err)
+    }
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ README.md
@@ -0,0 +1,1 @@
+note: api key tidak diupload
